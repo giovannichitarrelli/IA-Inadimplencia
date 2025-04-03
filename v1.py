52940a14d01bc76906a2297be75b0d5e9e460b41
GiovanniAssis <giovanni.chitarrelli@gmail.com>
2025-04-03 14:22:33 -0300

    feat: add security api key from v1

--- v1.py
+++ v1.py
@@ -2,8 +2,11 @@
 
 from openai import OpenAI
 import httpx
- 
-client = OpenAI(api_key="sk-461442f8d35c4fc7b6c543e48ac0c431", base_url="https://api.deepseek.com", http_client=httpx.Client(verify=False))
+import os 
+from dotenv import load_dotenv
+
+api_key=os.getenv("API_KEY")
+client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com", http_client=httpx.Client(verify=False))
 
 user_content = input("Como posso te ajudar hoje? ")
 
