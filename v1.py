73af6fc10c4d069cc93aae213526fbe58d7b66a2
GiovanniAssis <giovanni.chitarrelli@gmail.com>
2025-04-03 14:24:16 -0300

    fix: updated dotenv from v1

--- v1.py
+++ v1.py
@@ -5,6 +5,8 @@ import httpx
 import os 
 from dotenv import load_dotenv
 
+
+load_dotenv()
 api_key=os.getenv("API_KEY")
 client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com", http_client=httpx.Client(verify=False))
 
